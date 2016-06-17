0985fe1ee52886f85cde2d7786b6235c2fa44500
Peter L <peter@werl.me>
2016-06-16 19:09:14 -0700

    Revert "Fixing databases"

--- db/migrate/20160617000707_create_orders.rb
+++ db/migrate/20160617000707_create_orders.rb
@@ -1,8 +1,8 @@
 class CreateOrders < ActiveRecord::Migration
   def change
     create_table :orders do |t|
-      t.references :user
-      t.references :game
+      t.references :user_id
+      t.references :game_id
       t.timestamps
     end
   end
