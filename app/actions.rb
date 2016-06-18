4410472040c372d0cfd37e10ba4ba10f83d3c2d5
werl <peter@werl.me>
2016-06-18 16:42:29 -0700

    used wrong quotes

--- app/actions.rb
+++ app/actions.rb
@@ -143,7 +143,7 @@ post '/games/new' do
     client = Twilio::REST::Client.new ENV['TW_SSID'], ENV['TW_AUTH']
     client.account.messages.create({
       from: '+12044006394',
-      to:   '+1#{player.phone_numer}',
+      to:   "+1#{player.phone_numer}",
       body: 'Coffee Roulette'
     })
   end
